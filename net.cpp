83023fb3f0971894cb9cd559e4d29bc647f5247b
kuba <kuba.ostrowicki@interia.pl>
2015-08-08 18:45:05 +0200

    animation

---
+++ net.cpp
@@ -0,0 +1,38 @@
+#include <GL/glut.h> 
+
+GLfloat gfPosX = 0.0;
+GLfloat gfDeltaX = .0001;
+
+void Draw() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+		glVertex3f(gfPosX, 0.25, 0.0);
+		glVertex3f(1.0 - gfPosX, 0.75,0.0);
+	glEnd();
+	glFlush();
+	gfPosX += gfDeltaX;
+	if (gfPosX >= 1.0 || gfPosX <= 0.0) {
+		gfDeltaX = -gfDeltaX;
+	}
+	glutPostRedisplay();
+}
+
+void Initialize() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int main(int iArgc, char** cppArgv) {
+	glutInit(&iArgc, cppArgv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(200, 200);
+	glutCreateWindow("XoaX.net");
+	Initialize();
+	glutDisplayFunc(Draw);
+	glutMainLoop();
+	return 0;
+}
