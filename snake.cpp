e6f16c0ed390570ab3dedd20d76c50f84916f8a0
kuba <kuba.ostrowicki@interia.pl>
2015-08-09 17:29:05 +0200

    running head in test.cpp

--- snake.cpp
+++ snake.cpp
@@ -2,10 +2,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
+class snakeField{
+
+  float x;
+  float y;
+};
+
 float a, b;
+float c = 0.0;
+float d = 0.0;
+char actual;
+vector<float> wonszx;
+vector<float> wonszy;
+vector<snakeField> wonsz;
+
+void Timer(int iUnused)
+{
+    glutPostRedisplay();
+    glutTimerFunc(30, Timer, 0);
+    for (int i =0; i<wonszx.size(); i++){
+      wonszx[i] = wonszx[i] + 5;
+    }
+}
 
 void initGL() {
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -15,10 +38,47 @@ void displayBoard(){
   glClear (GL_COLOR_BUFFER_BIT);
 }
 
+/*
+void sortSnake(char direction){
+  if (direction == 'w' && actual != 's')
+    wonszy[0] += 5;
+  else if (direction == 's' && actual != 'w')
+    wonszy[0] -= 5;
+  else if (direction == 'a' && actual != 'd')
+    wonszx[0]
+}*/
+
+void countSnake(){
+
+}
+void lel(){
+  glColor3f (1.0, 1.0, 0.0);
+  glPointSize(10.0);
+    glBegin(GL_POINTS);
+      glVertex2f(c, d);
+      glVertex2f(120, 20);
+      glVertex2f(120, 9);
+    glEnd();
+    c = fmod((c+5), 800);
+    d = fmod((d+5), 600);
+}
+
+void drawSnake(){
+  glColor3f(1.0, 1.0, 0.0);
+  glPointSize(5.0);
+  glBegin(GL_POINTS);
+    glVertex2f(wonszx[0], wonszy[0]);
+    glColor3f(0.0, 1.0, 0.0);
+    for (int i =1; i<wonszx.size(); i++){
+      glVertex2f(wonszx[i], wonszy[i]);
+    }
+  glEnd();
+}
+
 void getField(){
   glColor3f (1.0, 0.0, 0.0);
-  float a = rand()%800;
-  float b = rand()%600;
+  //float a = rand()%800;
+  //float b = rand()%600;
 
   glPointSize(5.0);
   glBegin(GL_POINTS);
@@ -29,25 +89,31 @@ void getField(){
 void showGame(){
   displayBoard();
   getField();
+  //lel();
+  drawSnake();
   glFlush();
 }
 
 void keyboard(unsigned char key, int x, int y){
     switch (key){
-    case 'w':
-      //glClearColor (1.0, 0.0, 0.0, 0.0);
+    case 'd':
       a+=10;
       break;
  
-    case 's':
-      //glClearColor (0.0, 1.0, 0.0, 0.0);
+    case 'a':
       a -= 10.0;
     break;
+
+    case 'w':
+      b += 10;
+    break;
+
+    case 's':
+      b -= 10;
  
     default:
-         break;
+      break;
   }
-   glutPostRedisplay(); //request display() call ASAP
 }
 
 void reshape (int w, int h)
@@ -64,14 +130,25 @@ int main(int argc, char** argv) {
   a = rand()%800;
   b = rand()%600;
 
+  
+  wonszx.push_back(65);
+  wonszx.push_back(60);
+  wonszx.push_back(55);
+  wonszx.push_back(50);
+  wonszy.push_back(10);
+  wonszy.push_back(10);
+  wonszy.push_back(10);
+  wonszy.push_back(10);
+
   glutInit(&argc, argv);
   glutCreateWindow("leSnake");
   glutInitWindowPosition(50, 50);
   glutReshapeWindow(800, 600);
   initGL();
-  glutDisplayFunc(showGame); 
   glutReshapeFunc(reshape);
   glutKeyboardFunc(keyboard);
+  glutDisplayFunc(showGame);
+  Timer(0); 
   glutMainLoop();
   
   return 0;
--- test.cpp
+++ test.cpp
@@ -3,12 +3,17 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
+char dir = 'd';
 float a, b;
 float c = 0.0;
 float d = 0.0;
+char actual;
+vector<float> wonszx;
+vector<float> wonszy;
 
 void Timer(int iUnused)
 {
@@ -24,20 +29,38 @@ void displayBoard(){
   glClear (GL_COLOR_BUFFER_BIT);
 }
 
-void lel(){
-	glColor3f (1.0, 1.0, 0.0);
-	glPointSize(5.0);
-  	glBegin(GL_POINTS);
-    	glVertex2f(c, d);
-  	glEnd();
-  	c = fmod((c+5), 800);
-  	d = fmod((d+5), 600);
+void lel(char asd){
+  dir = asd;
+    if (dir == 'w')
+      wonszy[0] = abs(fmod(wonszy[0]+5, 600));
+    else if (dir == 's'){
+      if (wonszy[0]<0)
+        wonszy[0] = 600;
+      wonszy[0] = fmod(wonszy[0]-5, 600);
+    }
+    else if (dir == 'a'){
+      if (wonszx[0] < 0)
+        wonszx[0] = 800;
+      wonszx[0] = fmod(wonszx[0]-5, 800);
+    }
+    else if (dir == 'd')
+      wonszx[0] =abs(fmod(wonszx[0]+5, 800));
+}
+
+void drawSnake(){
+  glColor3f(1.0, 1.0, 0.0);
+  glPointSize(5.0);
+  glBegin(GL_POINTS);
+    glVertex2f(wonszx[0], wonszy[0]);
+    glColor3f(0.0, 1.0, 0.0);
+    for (int i =1; i<wonszx.size(); i++){
+      glVertex2f(wonszx[i], wonszy[i]);
+    }
+  glEnd();
 }
 
 void getField(){
   glColor3f (1.0, 0.0, 0.0);
-  //float a = rand()%800;
-  //float b = rand()%600;
 
   glPointSize(5.0);
   glBegin(GL_POINTS);
@@ -47,8 +70,9 @@ void getField(){
 
 void showGame(){
   displayBoard();
-  getField();
-  lel();
+  //getField();
+  lel(dir);
+  drawSnake();
   glFlush();
 }
 
@@ -70,9 +94,9 @@ void keyboard(unsigned char key, int x, int y){
       b -= 10;
  
     default:
-         break;
+      break;
   }
-   //glutPostRedisplay(); //request display() call ASAP
+    lel(key);
 }
 
 void reshape (int w, int h)
@@ -83,13 +107,25 @@ void reshape (int w, int h)
   gluOrtho2D (0.0, (GLdouble) w, 0.0, (GLdouble) h);
 }
 
-
 int main(int argc, char** argv) {
   srand( time( NULL ) );
 
   a = rand()%800;
   b = rand()%600;
 
+  
+  wonszx.push_back(65);
+  /*
+  wonszx.push_back(60);
+  wonszx.push_back(55);
+  wonszx.push_back(50);
+  */
+  wonszy.push_back(10);
+  /*
+  wonszy.push_back(10);
+  wonszy.push_back(10);
+  wonszy.push_back(10);
+  */
   glutInit(&argc, argv);
   glutCreateWindow("leSnake");
   glutInitWindowPosition(50, 50);
